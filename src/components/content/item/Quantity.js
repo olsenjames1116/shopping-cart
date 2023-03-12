ecee24fdbc392b76d6ce08002e44f52381c7f2fc
James Olsen <olsenjames1116@gmail.com>
2023-03-12 15:59:19 -0700

    Validity checked from quantity input
    If a user attempts to enter a value that is larger or greater than the
    min or max, the value will be set to the min or max.

--- src/components/content/item/Quantity.js
+++ src/components/content/item/Quantity.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 export default function Quantity({quantity, name}) {
+    function checkValidity(event) {
+        let input = parseInt(event.target.value)
+
+        if(input > 99) event.target.value = 99;
+        if(input < 0) event.target.value = 0;
+    }
+
     return (
         <form onSubmit={(event) => event.preventDefault()}>
-            <input className={name} type='number' id='quantity' min='0' max='999' placeholder={quantity} />
+            <input className={name} type='number' id='quantity' min='0' max='99' placeholder={quantity} onChange={checkValidity} />
         </form>
     );
 }
